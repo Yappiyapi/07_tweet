4a54a1b0edbbaf93057de8ae1a92a30ef46bd489
Yappiyapi <yynissy.718@gmail.com>
2020-03-16 13:59:11 +0900

    修正完了

--- show.php
+++ show.php
@@ -33,9 +33,9 @@ $tweet = $stmt->fetch(PDO::FETCH_ASSOC);
       <?php echo h($tweet['content']); ?><br>
       投稿日時: <?php echo h($tweet['created_at']); ?>
       <?php if($tweet['good'] == 0) : ?>
-        <a href="good.php?id=<?php echo h($tweet['id']) . "&good=0"; ?>" class="good-link"><?php echo '☆'; ?></a>
+        <a href="good.php?id=<?php echo h($tweet['id']) . "&good=1"; ?>" class="good-link"><?php echo '☆'; ?></a>
       <?php else : ?>
-        <a href="good.php?id=<?php echo h($tweet['id']) . "&good=1"; ?>" class="bad-link"><?php echo '★'; ?></a>
+        <a href="good.php?id=<?php echo h($tweet['id']) . "&good=0"; ?>" class="bad-link"><?php echo '★'; ?></a>
       <?php endif; ?>
       <a href="edit.php?id=<?php echo h($tweet['id']); ?>">[編集]</a>
       <a href="delete.php?id=<?php echo h($tweet['id']); ?>">[削除]</a>
